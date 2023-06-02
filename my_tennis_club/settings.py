6f8971d25b872ea2547ac87d9498c326de9a95ab
ElvisJin <elvis.jin.1995@gmail.com>
2023-06-02 17:38:32 +0900

    add test view

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c&$!0n1@96*gbcb7fqt&6*ifftje6xdlfmau#q*%wz8@q$fl&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
